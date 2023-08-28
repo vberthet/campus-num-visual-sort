4ac171efedb9c603036ca8ab9dcca5f9cb7a3a1d
Vincent Berthet <vincent.berthet@gmail.com>
2023-08-28 23:06:56 +0200

    Remove Bubble sort

--- src/main/java/edu/campusnum/visualsort/sort/BubbleSort.java
+++ src/main/java/edu/campusnum/visualsort/sort/BubbleSort.java
@@ -11,19 +11,6 @@ import edu.campusnum.visualsort.model.ObservableArray;
 public class BubbleSort implements SortAlgorithm{
     @Override
     public void sort(ObservableArray array) {
-        int n = array.getLength();
-        int i, j;
-        boolean swapped;
-        for (i = 0; i < n - 1; i++) {
-            swapped = false;
-            for (j = 0; j < n - i - 1; j++) {
-                if (array.get(j) > array.get(j + 1)) {
-                    array.swap(j, j + 1);
-                    swapped = true;
-                }
-            }
-            if (!swapped)
-                break;
-        }
+        throw new UnsupportedOperationException("Not implemented");
     }
 }
